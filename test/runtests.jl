faf8347bca61558ffa0c2ed6263495f701e6c0a8
Dominique Orban <dominique.orban@gmail.com>
2016-04-07 16:23:18 -0400

    initial tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,7 @@
+using Optimize
+using NLPModels
+using OptimizationProblems
 
+model = JuMPNLPModel(dixmaanj())
+stats = run_solver(:trunk, model, verbose=true)
+stats = run_solver(:lbfgs, model, verbose=true)
