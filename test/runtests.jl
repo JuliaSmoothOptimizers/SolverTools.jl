c679669ea12a97f0d11545a5d6399cba6d9afdd3
Dominique Orban <dominique.orban@gmail.com>
2016-09-03 11:51:17 -0400

    test solvers on AmplModel
    Closes #6

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,15 @@
 using Optimize
 using NLPModels
+using AmplNLReader
 using OptimizationProblems
 
-model = JuMPNLPModel(dixmaanj())
-stats = run_solver(:trunk, model, verbose=true)
-reset!(model)
-stats = run_solver(:lbfgs, model, verbose=true)
+models = [AmplModel("dixmaanj.nl"), JuMPNLPModel(dixmaanj())]
+solvers = [:trunk, :lbfgs]
+
+for model in models
+  for solver in solvers
+    stats = run_solver(solver, model, verbose=true)
+    assert(all([stats...] .>= 0))
+    reset!(model)
+  end
+end
