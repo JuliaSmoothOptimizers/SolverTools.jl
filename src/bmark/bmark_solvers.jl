943d451bedb615f463c7d6de03fc83f74e3d047f
Dominique Orban <dominique.orban@gmail.com>
2016-09-16 13:42:00 -0400

    pass kwargs along but not to profiles

--- src/bmark/bmark_solvers.jl
+++ src/bmark/bmark_solvers.jl
@@ -6,14 +6,14 @@ function bmark_solvers(solvers :: Vector{Symbol}, probs :: Vector{Symbol}, n :: 
                        format :: Symbol=:jump, kwargs...)
   stats = Dict{Symbol, Array{Int,2}}()
   for solver in solvers
-    stats[solver] = run_problems(solver, probs, n, format=format)
+    stats[solver] = run_problems(solver, probs, n, format=format; kwargs...)
   end
   return stats
 end
 
 "Plot a performance profile from a dict of statistics such as obtained from `bmark_solvers`."
 function profile_solvers(stats :: Dict{Symbol, Array{Int,2}};
-                         title :: AbstractString="", kwargs...)
+                         title :: AbstractString="")
   performance_profile(hcat([sum(p, 2) for p in values(stats)]...),
                       collect(AbstractString, [string(s) for s in keys(stats)]),
                       title=title)
@@ -22,5 +22,5 @@ end
 "Run a set of solvers on a set of problems and plot a performance profile."
 function bmark_and_profile(args...; kwargs...)
   stats = bmark_solvers(args...; kwargs...)
-  profile_solvers(stats; kwargs...)
+  profile_solvers(stats)
 end
