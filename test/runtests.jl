e5b4c26bd7e48d0fcb1ad5286800f9c0847077ce
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2017-06-09 20:55:05 -0300

    Update show and print

--- test/runtests.jl
+++ test/runtests.jl
@@ -50,6 +50,15 @@ end
 include("solvers/tron.jl")
 
 # Test ExecutionStats
-stats = ExecutionStats(:first_order, obj=1.0, opt=1e-12,
-                       solver_specific=Dict(:matvec=>10, :dot=>25, :axpy=>20))
+stats = ExecutionStats(:first_order, obj=1.0, opt=1e-12, sol=ones(100),
+                       solver_specific=Dict(:matvec=>10, :dot=>25, :axpy=>20,
+                                            :ray=>-1 ./ (1:100)))
 println(stats)
+open("teststats.out", "w") do f
+  println(f, stats)
+end
+
+println(stats, showvec=(io,x)->print(io,x))
+open("teststats.out", "a") do f
+  println(f, stats, showvec=(io,x)->print(io,x))
+end
