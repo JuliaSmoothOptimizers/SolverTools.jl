51f3871ba6dea984c00e2631b62b6261747ef1d1
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2017-07-18 10:20:59 -0300

    Implement solver TRON

---
+++ src/solver/tron.jl
@@ -0,0 +1,414 @@
+# Some parts of this code were adapted from
+# https://github.com/PythonOptimizers/NLP.py/blob/develop/nlp/optimize/tron.py
+
+using LinearOperators, NLPModels
+
+export tron, project!, project_step!, active, breakpoints
+
+"""`tron(nlp)`
+
+A pure Julia implementation of a trust-region solver for bound-constrained
+optimization:
+
+    min f(x)    s.t.    ℓ ≦ x ≦ u
+
+TRON is described in
+
+Chih-Jen Lin and Jorge J. Moré, *Newton's Method for Large Bound-Constrained
+Optimization Problems*, SIAM J. Optim., 9(4), 1100–1127, 1999.
+"""
+function tron(nlp :: AbstractNLPModel;
+              μ₀ :: Real=1e-2,
+              μ₁ :: Real=1.0,
+              σ :: Real=10.0,
+              verbose=false,
+              itmax :: Int=10_000*nlp.meta.nvar,
+              max_cgiter :: Int=nlp.meta.nvar,
+              cgtol = 0.1,
+              fmin :: Real = -1e32,
+              timemax :: Real=60.0,
+              mem :: Integer=5,
+              atol :: Real=1e-8,
+              rtol :: Real=1e-6,
+              fatol :: Real=0.0,
+              frtol :: Real=1e-12,
+              σ₁ :: Real=0.25,
+              σ₂ :: Real=0.5,
+              σ₃ :: Real=4.0,
+              η₀ :: Real=1e-4,
+              η₁ :: Real=0.25,
+              η₂ :: Real=0.75,
+              Δmax :: Real=1e6
+             )
+  ℓ = nlp.meta.lvar
+  u = nlp.meta.uvar
+  f(x) = obj(nlp, x)
+  g(x) = grad(nlp, x)
+  n = nlp.meta.nvar
+
+  iter = 0
+  start_time = time()
+  el_time = 0.0
+
+  # Preallocation
+  temp = zeros(n)
+  gpx = zeros(n)
+  xc = zeros(n)
+  gc = zeros(n)
+  Hs = zeros(n)
+
+  x = max.(ℓ, min.(nlp.meta.x0, u))
+  fx = f(x)
+  gx = g(x)
+  num_success_iters = 0
+
+  # Optimality measure
+  project_step!(gpx, x, -gx, ℓ, u)
+  πx = norm(gpx)
+  ϵ = atol + rtol * πx
+  optimal = πx <= ϵ
+  tired = iter >= itmax || el_time > timemax
+  unbounded = fx < fmin
+  stalled = false
+
+  αC = 1.0
+  Δ = min(max(1.0, 0.1*norm(πx)), 100)
+  if verbose
+    @printf("%4s  %9s  %7s  %7s  %7s  %s\n", "Iter", "f", "π", "Radius", "Ratio", "CG-status")
+    @printf("%4d  %9.2e  %7.1e  %7.1e\n", iter, fx, πx, Δ)
+  end
+  while !(optimal || tired || stalled)
+    # Current iteration
+    xc .= x
+    fc = fx
+    gc .= gx
+
+    # Model
+    H = hess_op!(nlp, x, temp)
+
+    # Cauchy starts here
+    αC, s = cauchy(x, H, gx, Δ, αC, ℓ, u, μ₀=μ₀, μ₁=μ₁, σ=σ)
+
+    s, cgits, cginfo = projected_newton!(x, H, gx, Δ, cgtol, s, ℓ, u, max_cgiter=max_cgiter)
+    slope, qs = compute_Hs_slope_qs!(Hs, H, s, gx)
+    Pred = -qs
+
+    fx = f(x)
+    Ared = fc - fx
+    if num_success_iters == 0
+      Δ = min(Δ, norm(s))
+    end
+
+    # Update the trust region
+    γ = fx - fc - slope
+    if γ <= 0.0
+      α = σ₃
+    else
+      α = max(σ₁, -0.5*slope/γ)
+    end
+
+    success = Ared >= η₀ * Pred
+
+    if Ared < η₀ * Pred
+      Δ = min(max(α, σ₁) * norm(s), σ₂ * Δ)
+    elseif Ared < η₁ * Pred
+      Δ = max(σ₁ * Δ, min(α * norm(s), σ₂ * Δ))
+    elseif Ared < η₂ * Pred
+      Δ = max(σ₁ * Δ, min(α * norm(s), σ₃ * Δ))
+    else
+      Δ = max(Δ, min(α * norm(s), σ₃ * Δ))
+    end
+
+    if success
+      num_success_iters += 1
+      gx = g(x)
+      project_step!(gpx, x, -gx, ℓ, u)
+      πx = norm(gpx)
+    end
+
+    # No post-iteration
+
+    if !success
+      fx = fc
+      x .= xc
+    end
+
+    iter += 1
+    el_time = time() - start_time
+    tired = iter >= itmax ||  el_time > timemax
+    optimal = πx <= ϵ || fx == -Inf
+
+    verbose && @printf("%4d  %9.2e  %7.1e  %7.1e  %7.1e  %s\n", iter, fx, πx, Δ, Ared/Pred, cginfo)
+  end
+
+  status = if tired
+    iter >= itmax ? "maximum number of iterations" : "maximum elapsed time"
+  elseif πx <= ϵ
+    "first-order stationary"
+  elseif fx == -Inf
+    "objective function is unbounded from below"
+  elseif stalled
+    "stalled"
+  else
+    "unknown"
+  end
+
+  return x, fx, πx, iter, optimal, tired, status, el_time
+end
+
+"""
+    nbrk, brkmin, brkmax = breakpoints(x, d, ℓ, u)
+
+Find the smallest and largest values of `α` such that `x + αd` lies on the
+boundary. `x` is assumed to be feasible. `nbrk` is the number of violated
+bounds in the given direction.
+"""
+function breakpoints(x::Vector, d::Vector, ℓ::Vector, u::Vector)
+  n = length(x)
+  pos = find( (d .> 0) .& (x .< u) )
+  neg = find( (d .< 0) .& (x .> ℓ) )
+
+  nbrk = length(pos) + length(neg)
+  nbrk == 0 && return 0, 0, 0
+
+  brkmin = Inf
+  brkmax = 0.0
+  if length(pos) > 0
+    @views steps = (u[pos] - x[pos])./d[pos]
+    brkmin = min.(brkmin, minimum(steps))
+    brkmax = max.(brkmax, maximum(steps))
+  end
+  if length(neg) > 0
+    @views steps = (ℓ[neg] - x[neg])./d[neg]
+    brkmin = min.(brkmin, minimum(steps))
+    brkmax = max.(brkmax, maximum(steps))
+  end
+  return nbrk, brkmin, brkmax
+end
+
+"""`slope, qs = compute_Hs_slope_qs!(Hs, H, s, g)`
+
+Computes
+
+    Hs = H * s
+    slope = dot(g,s)
+    qs = ¹/₂sᵀHs + slope
+"""
+function compute_Hs_slope_qs!(Hs::Vector, H::Union{Matrix,AbstractLinearOperator},
+                              s::Vector, g::Vector)
+  Hs = H * s
+  slope = dot(g,s)
+  qs = 0.5*dot(s, Hs) + slope
+  return slope, qs
+end
+
+"""`active(x, ℓ, u; rtol = 1e-8, atol = 1e-8)`
+
+Computes the active bounds at x, using tolerance `min(rtol * (uᵢ-ℓᵢ), atol)`.
+If ℓᵢ or uᵢ is not finite, only `atol` is used.
+"""
+function active(x::Vector, ℓ::Vector, u::Vector; rtol::Real = 1e-8, atol::Real = 1e-8)
+  A = Int[]
+  n = length(x)
+  for i = 1:n
+    δ = -Inf < ℓ[i] < u[i] < Inf ? min(rtol * (u[i]-ℓ[i]), atol) : atol
+    if ℓ[i] == x[i] == u[i]
+      push!(A, i)
+    elseif x[i] <= ℓ[i] + δ
+      push!(A, i)
+    elseif x[i] >= u[i] - δ
+      push!(A, i)
+    end
+  end
+  return A
+end
+
+"""`project!(y, x, ℓ, u)`
+
+Projects `x` into bounds `ℓ` and `u`, in the sense of
+`yᵢ = max(ℓᵢ, min(xᵢ, uᵢ))`.
+"""
+function project!(y :: Vector, x :: Vector, ℓ :: Vector, u :: Vector)
+  y .= max.(ℓ, min.(x, u))
+end
+
+"""`project_step!(y, x, d, ℓ, u)`
+
+Computes the projected direction `y = P(x + d) - x`.
+"""
+function project_step!(y::Vector, x::Vector, d::Vector, ℓ::Vector, u::Vector)
+  y .= x + d
+  project!(y, y, ℓ, u)
+  y .-= x
+end
+
+"""`x, s = projected_line_search!(x, H, g, d, ℓ, u; μ₀ = 1e-2)`
+
+Performs a projected line search, searching for a direction
+`s = P(x + t * d) - x` such that
+
+    0.5sᵀHs + sᵀg ≦ μ₀sᵀg,
+
+but without leaving the first face found by `x + d`.  Backtracking is
+performed from t = 1.0. `x` is updated in place.
+"""
+function projected_line_search!(x::Vector, H::Union{Matrix,AbstractLinearOperator}, g::Vector, d::Vector, ℓ::Vector, u::Vector; μ₀::Real = 1e-2)
+  α = 1.0
+  _, brkmin, _ = breakpoints(x, d, ℓ, u)
+  nsteps = 0
+  n = length(x)
+
+  s = zeros(n)
+  Hs = zeros(n)
+
+  search = true
+  while search && α > brkmin
+    nsteps += 1
+    project_step!(s, x, α*d, ℓ, u)
+    slope, qs = compute_Hs_slope_qs!(Hs, H, s, g)
+    if qs <= μ₀*slope
+      search = false
+    else
+      α *= 0.5
+    end
+  end
+  if α < 1.0 && α < brkmin
+    α = brkmin
+  end
+
+  αd = α*d
+  project_step!(s, x, αd, ℓ, u)
+  project!(x, x + αd, ℓ, u)
+
+  return x, s
+end
+
+"""`α, s = cauchy(x, H, g, Δ, ℓ, u; μ₀ = 1e-2, μ₁ = 1.0, σ=10.0)`
+
+Computes a Cauchy step `s = P(x - α g) - x` for
+
+    min  q(s) = ¹/₂sᵀHs + gᵀs     s.t.    ‖s‖ ≦ μ₁Δ,  ℓ ≦ x + s ≦ u,
+
+with the sufficient decrease condition
+
+    q(s) ≦ μ₀sᵀg.
+"""
+function cauchy(x::Vector, H::Union{Matrix,AbstractLinearOperator}, g::Vector,
+                Δ::Real, α::Real, ℓ::Vector, u::Vector;
+                μ₀::Real = 1e-2, μ₁::Real = 1.0, σ::Real = 10.0)
+  # TODO: Use brkmin to care for g direction
+  _, _, brkmax = breakpoints(x, -g, ℓ, u)
+  n = length(x)
+  s = zeros(n)
+  Hs = zeros(n)
+
+  project_step!(s, x, -α*g, ℓ, u)
+
+  # Interpolate or extrapolate
+  s_norm = norm(s)
+  if s_norm > μ₁*Δ
+    interp = true
+  else
+    slope, qs = compute_Hs_slope_qs!(Hs, H, s, g)
+    interp = qs >= μ₀ * slope
+  end
+
+  if interp
+    search = true
+    while search
+      α /= σ
+      project_step!(s, x, -α*g, ℓ, u)
+      s_norm = norm(s)
+      if s_norm <= μ₁*Δ
+        slope, qs = compute_Hs_slope_qs!(Hs, H, s, g)
+        search = qs >= μ₀ * slope
+      end
+      # TODO: Correctly assess why this fails
+      if α < 1e-200
+        throw("α too small (qs = $qs, slope = $slope)")
+        #stalled = true
+        #status = "α too small"
+        #break
+      end
+    end
+  else
+    search = true
+    αs = α
+    while search && α <= brkmax
+      α *= σ
+      project_step!(s, x, -α*g, ℓ, u)
+      s_norm = norm(s)
+      if s_norm <= μ₁*Δ
+        slope, qs = compute_Hs_slope_qs!(Hs, H, s, g)
+        if qs <= μ₀ * slope
+          αs = α
+        end
+      else
+        search = false
+      end
+    end
+    # Recover the last successful step
+    α = αs
+    s = project_step!(s, x, -α*g, ℓ, u)
+    slope, qs = compute_Hs_slope_qs!(Hs, H, s, g)
+  end
+  return α, s
+end
+
+"""`projected_newton!(x, H, g, Δ, gctol, s, max_cgiter, ℓ, u)`
+
+Compute an approximate solution `d` for
+
+    min q(d) = ¹/₂dᵀHs + dᵀg    s.t.    ℓ ≦ x + d ≦ u,  ‖d‖ ≦ Δ
+
+starting from `s`.  The steps are computed using the conjugate gradient method
+projected on the active bounds.
+"""
+function projected_newton!(x::Vector, H::Union{Matrix,AbstractLinearOperator},
+                          g::Vector, Δ::Real, cgtol::Real, s::Vector,
+                          ℓ::Vector, u::Vector;
+                          max_cgiter::Int = max(50,length(x)))
+  n = length(x)
+  status = ""
+  # Projected Newton Step
+  exit_optimal = false
+  exit_pcg = false
+  exit_itmax = false
+  iters = 0
+  project!(x, x + s, ℓ, u)
+  while !(exit_optimal || exit_pcg || exit_itmax)
+    ifree = setdiff(1:n, active(x, ℓ, u))
+    if length(ifree) == 0
+      exit_optimal = true
+      continue
+    end
+    Z = opExtension(ifree, n)
+    Hs = H * s
+    @views wa = g[ifree]
+    @views gfree = Hs[ifree] + wa
+    gfnorm = norm(wa)
+
+    ZHZ = Z' * H * Z
+    st, stats = Krylov.cg(ZHZ, -gfree, radius=Δ, rtol=cgtol, atol=0.0,
+                          itmax=max_cgiter)
+    iters += length(stats.residuals)
+    status = stats.status
+
+    # Projected line search
+    # TODO: Don't create new arrays here
+    xfree, w = projected_line_search!(x[ifree], ZHZ, gfree, st, ℓ[ifree], u[ifree])
+    x[ifree] = xfree
+    @views s[ifree] += w
+
+    @views gfree = Hs[ifree] + g[ifree]
+    if norm(gfree) <= cgtol * gfnorm
+      exit_optimal = true
+    elseif status == "on trust-region boundary"
+      exit_pcg = true
+    elseif iters >= max_cgiter
+      exit_itmax = true
+    end
+  end
+
+  return s, iters, status
+end
---
+++ test/solvers/tron.jl
@@ -0,0 +1,274 @@
+using CUTEst, LinearOperators
+
+@testset "Test auxiliary functions" begin
+  for t = 1:100
+    x = rand(5)
+    l = rand(5)
+    u = rand(5)
+    y = zeros(5)
+    y_loop = zeros(5)
+    project!(y, x, l, u)
+    for i = 1:5
+      y_loop[i] = max(l[i], min(x[i], u[i]))
+    end
+    @test y == y_loop
+
+    d = ones(5)
+    y_loop = zeros(5)
+    project_step!(y, x, d, l, u)
+    for i = 1:5
+      y_loop[i] = max(l[i], min(x[i] + d[i], u[i])) - x[i]
+    end
+    @test y == y_loop
+  end
+
+  x = zeros(6)
+  l = [-Inf; 0.0; -Inf; 0.0; -Inf; -1e-12]
+  u = [ 0.0; Inf;  Inf; 0.0; 1e-12;   Inf]
+  @test active(x, l, u) == [1; 2; 4; 5; 6]
+  @test active(x, l, u, rtol=0.0, atol=0.0) == [1; 2; 4]
+
+  x = ones(2)
+  l = zeros(2)
+  u = 2*ones(2)
+  @test breakpoints(x, ones(2), l, u) == (2, 1.0, 1.0)
+  @test breakpoints(x, [0.5; 1.0], l, u) == (2, 1.0, 2.0)
+  @test breakpoints(x, [0.0; 1.0], l, u) == (1, 1.0, 1.0)
+end
+
+@testset "Simple test" begin
+  @testset "No bounds" begin
+    x0 = [1.0; 2.0]
+    f(x) = dot(x,x)/2
+
+    nlp = ADNLPModel(f, x0, lvar=[-Inf; -Inf], uvar=[Inf; Inf])
+
+    x, fx, π, iter, optimal, tired, status = tron(nlp)
+    @test isapprox(x, zeros(2), rtol=1e-6)
+    @test isapprox(fx, 0.0, rtol=1e-12)
+    @test π < 1e-6
+    @test optimal == true
+  end
+
+  @testset "Bounds" begin
+    x0 = [1.0; 2.0]
+    f(x) = dot(x,x)/2
+    l = [0.5; 0.25]
+    u = [1.2; 1.5]
+
+    nlp = ADNLPModel(f, x0, lvar=l, uvar=u)
+
+    x, fx, π, iter, optimal, tired, status = tron(nlp)
+    @test x ≈ l
+    @test fx == f(l)
+    @test π < 1e-6
+    @test optimal == true
+  end
+
+  @testset "Rosenbrock" begin
+    x0 = [-1.2; 1.0]
+    f(x) = 1.0 + (x[1]-1)^2 + 100*(x[2]-x[1]^2)^2
+
+    nlp = ADNLPModel(f, x0)
+
+    x, fx, π, iter, optimal, tired, status = tron(nlp)
+    @test isapprox(x, [1.0;1.0], rtol=1e-3)
+    @test isapprox(fx, 1.0, rtol=1e-5)
+    @test π < 1e-6
+    @test optimal == true
+  end
+
+  @testset "Rosenbrock inactive bounds" begin
+    l = [0.5; 0.25]
+    u = [1.2; 1.5]
+    x0 = (l+u)/2
+    f(x) = 1.0 + (x[1]-1)^2 + 100*(x[2]-x[1]^2)^2
+
+    nlp = ADNLPModel(f, x0, lvar=l, uvar=u)
+
+    x, fx, π, iter, optimal, tired, status = tron(nlp)
+    @test isapprox(x, [1.0;1.0], rtol=1e-3)
+    @test isapprox(fx, 1.0, rtol=1e-5)
+    @test π < 1e-6
+    @test optimal == true
+  end
+
+  @testset "Rosenbrock active bounds" begin
+    l = [0.5; 0.25]
+    u = [0.9; 1.5]
+    x0 = (l+u)/2
+    f(x) = 1.0 + (x[1]-1)^2 + 100*(x[2]-x[1]^2)^2
+
+    nlp = ADNLPModel(f, x0, lvar=l, uvar=u)
+
+    sol = [0.9; 0.81]
+
+    x, fx, π, iter, optimal, tired, status = tron(nlp)
+    @test isapprox(x, sol, rtol=1e-3)
+    @test isapprox(fx, f(sol), rtol=1e-5)
+    @test π < 1e-6
+    @test optimal == true
+  end
+end
+
+@testset "Fixed variables" begin
+  @testset "One fixed" begin
+    x0 = ones(3)
+    l = [1.0; 0.0; 0.0]
+    u = [1.0; 2.0; 2.0]
+    f(x) = 0.5*dot(x - 3, x - 3)
+    nlp = ADNLPModel(f, x0, lvar=l, uvar=u)
+    x, fx, π, iter, optimal, tired, status = tron(nlp)
+    @test optimal == true
+    @test π < 1e-6
+    @test isapprox(x, [1.0; 2.0; 2.0], rtol=1e-3)
+    @test x[1] == 1.0
+  end
+
+  @testset "All fixed" begin
+    n = 100
+    x0 = zeros(n)
+    l = 0.9*ones(n)
+    u = copy(l)
+    f(x) = sum(x.^4)
+    nlp = ADNLPModel(f, x0, lvar=l, uvar=u)
+    x, fx, π, iter, optimal, tired, status = tron(nlp)
+    @test optimal == true
+    @test π == 0.0
+    @test x == l
+  end
+end
+
+@testset "Larger test" begin
+  n = 10
+  # TODO: Check the bounds on the errors
+  (Q,R) = qr(rand(n,n))
+  Qop = LinearOperator(Q)
+  Λ = linspace(1e-2, 1.0, n)
+  B = Qop'*opDiagonal(Λ)*Qop
+  r = ones(n)
+  x0 = r + 1./Λ
+  @testset "Quadratic unconstrained" begin
+    for t = 1:10
+      f(x) = 1.0 + 0.5 * dot(x-r, B * (x-r))
+
+      nlp = ADNLPModel(f, x0)
+
+      x, fx, π, iter, optimal, tired, status = tron(nlp)
+      normg0 = norm(B*(x0-r))
+      @test norm(x - r) < 1e-4 * normg0
+      @test isapprox(fx, 1.0, rtol=1e-6)
+      @test π < 1e-6 * normg0
+      @test optimal == true
+    end
+  end
+
+  @testset "Positive quadratic with bounds" begin
+    for t = 1:10
+      # Create a problem so that the solution is known
+      r = zeros(n)
+      l, u, λl, λu = -1-rand(n), 1+rand(n), zeros(n), zeros(n)
+      for i = 1:n
+        rnd = rand()
+        if rnd < 1//3
+          r[i] = l[i]
+          λl[i] = 1 + rand()
+        elseif rnd < 2//3
+          r[i] = u[i]
+          λu[i] = 1 + rand()
+        else
+          r[i] = l[i] + (0.1 + 0.8*rand())*(u[i] - l[i])
+        end
+      end
+      v = -B*r - λu + λl
+      x0 = [l[i] + rand() * (u[i] - l[i]) for i = 1:n]
+      f(x) = 0.5 * dot(x, B*x) + dot(x, v)
+
+      nlp = ADNLPModel(f, x0, lvar=l, uvar=u)
+
+      x, fx, π, iter, optimal, tired, status = tron(nlp)
+      normg0 = norm(B*(x0-r))
+
+      @test norm(x - r) < 1e-3
+      @test isapprox(fx, f(r), rtol=1e-4)
+      @test π < 1e-4
+      @test optimal == true
+      @test iter <= 10000
+      @test status == "first-order stationary"
+    end
+  end
+end
+
+@testset "Problems" begin
+  n = 10
+  x0 = 10*ones(n)
+  f(x) = 1e7*sum(exp.(x))
+
+  nlp = ADNLPModel(f, x0)
+
+  @testset "Iteration limit" begin
+    x, fx, π, iter, optimal, tired, status = tron(nlp, itmax=1)
+    @test iter == 1
+    @test tired == true
+  end
+
+  @testset "Time limit" begin
+    x, fx, π, iter, optimal, tired, status, el_time = tron(nlp, timemax=0)
+    @test el_time > 0
+    @test tired == true
+  end
+
+  @testset "x0 outside box" begin
+    l = rand(n)
+    u = l + rand(n)
+    x0 = u + rand(n)
+    nlp = ADNLPModel(x->dot(x,x), x0, lvar=l, uvar=u)
+    x, fx, π, iter, optimal, tired, status = tron(nlp)
+    @test norm(x - l) < 1e-3
+    @test isapprox(fx, dot(l,l), rtol=1e-5)
+    @test π < 1e-4
+    @test optimal == true
+  end
+end
+
+@testset "Extended Rosenbrock" begin
+  n = 30
+  function f(x)
+    fx = 1.0
+    for i = 1:n-1
+      fx += 100*(x[i+1] - x[i]^2)^2 + (x[i] - 1)^2
+    end
+    return fx
+  end
+  x0 = [i/(n+1) for i = 1:n]
+
+  nlp = ADNLPModel(f, x0)
+
+  x, fx, π, iter, optimal, tired, status = tron(nlp, timemax=600)
+  @test isapprox(x, ones(n), rtol=1e-5*n)
+  @test isapprox(fx, 1.0, rtol=1e-3)
+  @test π < 1e-3*n
+  @test optimal == true
+
+  nlp = ADNLPModel(f, x0, lvar=zeros(n), uvar=0.3*ones(n))
+
+  x, fx, π, iter, optimal, tired, status = tron(nlp, timemax=600)
+  @test π < 1e-3*n
+  @test optimal == true
+end
+
+@testset "CUTEst" begin
+  problems = CUTEst.select(max_var=10, max_con=0, only_bnd_var=true)
+  @printf("%8s  %5s  %4s  %9s  %9s  %9s  %6s  %6s  %6s  %6s  %s\n",
+          "Problem", "n", "type", "f(x)", "π", "time", "it", "#f", "#g", "#Hp", "status")
+  for p in problems
+    nlp = CUTEstModel(p)
+    x, fx, π, iter, optimal, tired, status, el_time = tron(nlp, timemax=3.0)
+    finalize(nlp)
+
+    ctype = length(nlp.meta.ifree) == nlp.meta.nvar ? "unc" : "bnd"
+    @printf("%8s  %5d  %4s  %9.2e  %9.2e  %9.2e  %6d  %6d  %6d  %6d  %s\n", p,
+            nlp.meta.nvar, ctype, fx, π, el_time, iter, neval_obj(nlp), neval_grad(nlp),
+            neval_hprod(nlp), status)
+  end
+end
