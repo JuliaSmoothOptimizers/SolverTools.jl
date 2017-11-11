fc21c8a4fd96d4209cddc430eccd2fa5465f71bd
Dominique Orban <dominique.orban@gmail.com>
2017-11-11 17:16:20 -0500

    update trunk driver for logging

--- examples/run_trunk.jl
+++ examples/run_trunk.jl
@@ -1,6 +1,10 @@
 using Optimize
 using NLPModels
 using OptimizationProblems
+using MiniLogging
+
+MiniLogging.basic_config(MiniLogging.INFO; date_format="%Y-%m-%d %H:%M:%S")
+trunklogger = get_logger("optimize.trunk")
 
 model = MathProgNLPModel(dixmaanj())
-stats = solve_problem(trunk, model, verbose=true)
+stats = solve_problem(trunk, model)
