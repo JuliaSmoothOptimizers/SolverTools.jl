f96e31b07d767e3683d3b35ac597e4aa8c601b46
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2017-12-09 22:50:14 -0200

    Make nlp a mandatory argument on ExecutionStats

--- test/test_stats.jl
+++ test/test_stats.jl
@@ -1,5 +1,6 @@
 function test_stats()
-  stats = GenericExecutionStats(:first_order, objective=1.0, dual_feas=1e-12,
+  nlp = ADNLPModel(x->dot(x,x), zeros(2))
+  stats = GenericExecutionStats(:first_order, nlp, objective=1.0, dual_feas=1e-12,
                          solution=ones(100),
                          solver_specific=Dict(:matvec=>10, :dot=>25,
                                               :axpy=>20, :ray=>-1 ./ (1:100)))
