04b5ba712c0822251e7d9a316c565004769ef5f2
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2018-03-21 11:39:45 -0300

    Fix statsgetfield and increase coverage

--- test/test_stats.jl
+++ test/test_stats.jl
@@ -1,10 +1,14 @@
 function test_stats()
   nlp = ADNLPModel(x->dot(x,x), zeros(2))
   stats = GenericExecutionStats(:first_order, nlp, objective=1.0, dual_feas=1e-12,
-                         solution=ones(100),
+                         solution=ones(100), iter=10,
                          solver_specific=Dict(:matvec=>10, :dot=>25,
+                                              :empty_vec=>[],
+                                              :small_vec=>[2.0;3.0],
                                               :axpy=>20, :ray=>-1 ./ (1:100)))
 
+  show(stats)
+  print(stats)
   println(stats)
   open("teststats.out", "w") do f
     println(f, stats)
@@ -14,6 +18,14 @@ function test_stats()
   open("teststats.out", "a") do f
     println(f, stats, showvec=(io,x)->print(io,x))
   end
+
+  line = [:status, :neval_obj, :objective, :iter]
+  for field in line
+    value = statsgetfield(stats, field)
+    println("$field -> $value")
+  end
+  println(statshead(line))
+  println(statsline(stats, line))
 end
 
 test_stats()
