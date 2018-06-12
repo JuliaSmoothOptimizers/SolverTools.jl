65dd9617b160373c3ae9f949f9f7d72c0717fb50
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2018-06-12 20:20:46 -0300

    Update benchmark and profile tools to use ExecutionStats

--- src/bmark/bmark_solvers.jl
+++ src/bmark/bmark_solvers.jl
@@ -1,6 +1,8 @@
-export bmark_solvers, profile_solvers, bmark_and_profile
+export bmark_solvers, profile_solvers,
+       bmark_and_profile
 
 
+optimizelogger = get_logger("optimize")
 """
     bmark_solvers(solvers :: Vector{Function}, args...; kwargs...)
 
@@ -14,11 +16,13 @@ Run a set of solvers on a set of problems.
 Any keyword argument accepted by `solve_problems()`
 
 #### Return value
-A Dict{Symbol, Array{Int,2}} of statistics.
+A Dict{Symbol, AbstractExecutionStats} of statistics.
 """
 function bmark_solvers(solvers :: Dict{Symbol,Function}, args...; kwargs...)
-  stats = Dict{Symbol, Array{Int,2}}()
-  for (name, solver) in solvers
+  stats = Dict{Symbol, Array{AbstractExecutionStats,1}}()
+  for (name,solver) in solvers
+    @info(optimizelogger,
+          @printf("running %s\n", string(solver)))
     stats[name] = solve_problems(solver, args...; kwargs...)
   end
   return stats
@@ -38,24 +42,40 @@ bmark_and_profile(args...; kwargs...) = error("BenchmarkProfiles is required for
   * `stats`: a dict of statistics such as obtained from `bmark_solvers()`
 
   #### Keyword arguments
-  Any keyword argument accepted by `BenchmarkProfiles.performance_profile()`.
+  * `cost`: a function `cost(::AbstractExecutionStats)` returning a positive cost
+    value. Usually, time or number of function evaluations.
+  * Any keyword argument accepted by `BenchmarkProfiles.performance_profile()`.
 
   #### Return value
   A profile as returned by `performance_profile()`.
+
+  #### Cost functions
+
+  The default cost function is the number of function evaluations, i.e.,
+
+      cost = stat->sum_counters(stat.counters)
+
+  Another commonly used option is the elapsed time:
+
+      cost = stat->stat.elapsed_time
   """
-  function profile_solvers{T,n}(stats :: Dict{Symbol, Array{T,n}}; kwargs...)
+  function profile_solvers(stats :: Dict{Symbol, Array{AbstractExecutionStats,1}};
+                           cost :: Function = stat->sum_counters(stat.counters),
+                           kwargs...)
     args = Dict(kwargs)
     if haskey(args, :title)
       args[:title] *= @sprintf(" (%d problems)", size(stats[first(keys(stats))], 1))
     end
-    BenchmarkProfiles.performance_profile(hcat([sum(p, 2) for p in values(stats)]...),
-                                          collect(String, [string(s) for s in keys(stats)]); args...)
+    solvers = keys(stats)
+    np, ns = length(stats[first(solvers)]), length(solvers)
+    P = [stats[s][p].status == :first_order ? cost(stats[s][p]) : -1 for p = 1:np, s in solvers]
+    BenchmarkProfiles.performance_profile(P, map(string, solvers); args...)
   end
 
   """
       bmark_and_profile(args...;
-                        bmark_args :: Dict{Symbol, Any}=Dict{Symbol,Any}(),
-                        profile_args :: Dict{Symbol, Any}=Dict{Symbol,Any}())
+                        bmark_args :: Dict{Symbol, <: Any}=Dict{Symbol,Any}(),
+                        profile_args :: Dict{Symbol, <: Any}=Dict{Symbol,Any}())
 
   Run a set of solvers on a set of problems and plot a performance profile.
 
@@ -69,12 +89,22 @@ bmark_and_profile(args...; kwargs...) = error("BenchmarkProfiles is required for
   #### Return value
   * A Dict{Symbol, Array{Int,2}} of statistics
   * a profile as returned by `performance_profile()`.
+
+  #### Cost functions
+
+  The default cost function is the number of function evaluations, i.e.,
+
+      cost = stat->sum_counters(stat.counters)
+
+  Another commonly used option is the elapsed time:
+
+      cost = stat->stat.elapsed_time
   """
-  function bmark_and_profile(args...;
-                             bmark_args :: Dict{Symbol, Any}=Dict{Symbol,Any}(),
-                             profile_args :: Dict{Symbol, Any}=Dict{Symbol,Any}())
+  @compat function bmark_and_profile(args...; cost :: Function = stat->sum_counters(stat.counters),
+                             bmark_args :: Dict{Symbol, <: Any}=Dict{Symbol,Any}(),
+                             profile_args :: Dict{Symbol, <: Any}=Dict{Symbol,Any}())
     stats = bmark_solvers(args...; bmark_args...)
-    profiles = profile_solvers(stats; profile_args...)
+    profiles = profile_solvers(stats, cost=cost; profile_args...)
     return stats, profiles
   end
 end
