f0c4a65edaca326856c06a140a56463b74a1d642
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2019-03-05 00:21:24 -0300

    Add primal_feas to stats and improve tests

--- test/test_stats.jl
+++ test/test_stats.jl
@@ -1,3 +1,20 @@
+function dummy_solver(nlp :: AbstractNLPModel;
+                      x :: AbstractVector = nlp.meta.x0,
+                      atol :: Real = sqrt(eps(eltype(x))),
+                      rtol :: Real = sqrt(eps(eltype(x))),
+                      max_eval :: Int = 1000,
+                      max_time :: Float64 = 30.0,
+                      max_iter :: Int = 1000
+                     )
+
+  return GenericExecutionStats(:first_order, nlp,
+                               objective=obj(nlp, x),
+                               dual_feas=norm(grad(nlp, x)),
+                               primal_feas=nlp.meta.ncon > 0 ? norm(cons(nlp, x)) : zero(eltype(x)),
+                               solution=x,
+                              )
+end
+
 function test_stats()
   nlp = ADNLPModel(x->dot(x,x), zeros(2))
   stats = GenericExecutionStats(:first_order, nlp, objective=1.0, dual_feas=1e-12,
@@ -27,7 +44,50 @@ function test_stats()
   println(statshead(line))
   println(statsline(stats, line))
 
-  @test_throws Exception GenericExecutionStats(:unkwown, nlp, bad=true)
+  @testset "Testing inference" begin
+    for T in (Float16, Float32, Float64, BigFloat)
+      nlp = ADNLPModel(x->dot(x, x), ones(T, 2))
+
+      stats = GenericExecutionStats(:first_order, nlp)
+      @test stats.status == :first_order
+      @test typeof(stats.objective) == T
+      @test typeof(stats.dual_feas) == T
+      @test typeof(stats.primal_feas) == T
+
+      nlp = ADNLPModel(x->dot(x, x), ones(T, 2), c=x->[sum(x)-1], lcon=[0.0], ucon=[0.0])
+
+      stats = GenericExecutionStats(:first_order, nlp)
+      @test stats.status == :first_order
+      @test typeof(stats.objective) == T
+      @test typeof(stats.dual_feas) == T
+      @test typeof(stats.primal_feas) == T
+    end
+  end
+
+  @testset "Test throws" begin
+    @test_throws Exception GenericExecutionStats(:bad, nlp)
+    @test_throws Exception GenericExecutionStats(:unkwown, nlp, bad=true)
+  end
+
+  @testset "Testing Dummy Solver with multi-precision" begin
+    for T in (Float16, Float32, Float64, BigFloat)
+      nlp = ADNLPModel(x->dot(x, x), ones(T, 2))
+
+      stats = dummy_solver(nlp)
+      @test stats.status == :first_order
+      @test typeof(stats.objective) == T
+      @test typeof(stats.dual_feas) == T
+      @test typeof(stats.primal_feas) == T
+
+      nlp = ADNLPModel(x->dot(x, x), ones(T, 2), c=x->[sum(x)-1], lcon=[0.0], ucon=[0.0])
+
+      stats = dummy_solver(nlp)
+      @test stats.status == :first_order
+      @test typeof(stats.objective) == T
+      @test typeof(stats.dual_feas) == T
+      @test typeof(stats.primal_feas) == T
+    end
+  end
 end
 
 test_stats()
