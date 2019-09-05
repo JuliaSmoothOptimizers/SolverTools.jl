223a841cac02cef9b9f4b362a205dd4c6ed43ce4
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2019-09-05 18:33:43 -0300

    Allow callables on bmark functions and add simple bmark tests

---
+++ test/test_bmark.jl
@@ -0,0 +1,28 @@
+using DataFrames
+
+mutable struct CallableSolver
+end
+
+function (solver :: CallableSolver)(nlp :: AbstractNLPModel; kwargs...)
+  return GenericExecutionStats(:unknown, nlp)
+end
+
+function test_bmark()
+  @testset "Testing bmark" begin
+    problems = [ADNLPModel(x -> sum(x.^k), ones(2k), name="Sum of power $k") for k = 2:4]
+    callable = CallableSolver()
+    stats = solve_problems(dummy_solver, problems)
+    @test stats isa DataFrame
+
+    solve_problems(callable, problems)
+
+    solvers = Dict(:dummy => dummy_solver, :callable => callable)
+    stats = bmark_solvers(solvers, problems)
+    @test stats isa Dict{Symbol, DataFrame}
+    for k in keys(solvers)
+      @test haskey(stats, k)
+    end
+  end
+end
+
+test_bmark()
