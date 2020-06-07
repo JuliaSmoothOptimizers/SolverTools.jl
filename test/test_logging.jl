cedf2b46bfe95e7d44ed941ca11fc6a7ed61ddf5
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2020-06-07 00:16:49 -0300

    Update to support NLPModels 0.13 and update version to 0.2.0

--- test/test_logging.jl
+++ test/test_logging.jl
@@ -1,6 +1,6 @@
 function test_logging()
   nlps = [ADNLPModel(x -> sum(x.^k), ones(2k), name="Sum of power $k") for k = 2:4]
-  push!(nlps, ADNLPModel(x -> dot(x, x), ones(2), c=x->[sum(x)-1], lcon=[0.0], ucon=[0.0], name="linquad"))
+  push!(nlps, ADNLPModel(x -> dot(x, x), ones(2), x->[sum(x)-1], [0.0], [0.0], name="linquad"))
 
   @info "Testing logger"
   log_header([:col_float, :col_int, :col_symbol, :col_string], [Float64, Int, Symbol, String])
