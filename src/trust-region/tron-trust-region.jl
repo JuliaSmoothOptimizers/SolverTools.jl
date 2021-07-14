c59854d405739bdb0cc314ef9cd83e75675c5e65
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2021-07-14 19:36:19 -0300

    Parametrize TrustRegion and modify to allow pre-allocation

--- src/trust-region/tron-trust-region.jl
+++ src/trust-region/tron-trust-region.jl
@@ -1,20 +1,23 @@
 export TRONTrustRegion
 
 """Trust region used by TRON"""
-mutable struct TRONTrustRegion <: AbstractTrustRegion
-  initial_radius::AbstractFloat
-  radius::AbstractFloat
-  max_radius::AbstractFloat
-  acceptance_threshold::AbstractFloat
-  decrease_threshold::AbstractFloat
-  increase_threshold::AbstractFloat
-  large_decrease_factor::AbstractFloat
-  small_decrease_factor::AbstractFloat
-  increase_factor::AbstractFloat
-  ratio::AbstractFloat
-  quad_min::AbstractFloat
+mutable struct TRONTrustRegion{T, V} <: AbstractTrustRegion{T, V}
+  initial_radius::T
+  radius::T
+  max_radius::T
+  acceptance_threshold::T
+  decrease_threshold::T
+  increase_threshold::T
+  large_decrease_factor::T
+  small_decrease_factor::T
+  increase_factor::T
+  ratio::T
+  quad_min::T
+  gt::V
+  good_grad::Bool
 
   function TRONTrustRegion(
+    gt::V,
     initial_radius::T;
     max_radius::T = one(T) / sqrt(eps(T)),
     acceptance_threshold::T = T(1.0e-4),
@@ -23,7 +26,7 @@ mutable struct TRONTrustRegion <: AbstractTrustRegion
     large_decrease_factor::T = T(0.25),
     small_decrease_factor::T = T(0.5),
     increase_factor::T = T(4),
-  ) where {T <: AbstractFloat}
+  ) where {T, V}
     initial_radius > 0 || (initial_radius = one(T))
     max_radius > initial_radius || throw(TrustRegionException("Invalid initial radius"))
     (0 < acceptance_threshold < decrease_threshold < increase_threshold < 1) ||
@@ -31,7 +34,7 @@ mutable struct TRONTrustRegion <: AbstractTrustRegion
     (0 < large_decrease_factor < small_decrease_factor < 1 < increase_factor) ||
       throw(TrustRegionException("Invalid decrease/increase factors"))
 
-    return new(
+    return new{T, V}(
       initial_radius,
       initial_radius,
       max_radius,
@@ -43,27 +46,32 @@ mutable struct TRONTrustRegion <: AbstractTrustRegion
       increase_factor,
       zero(T),
       zero(T),
+      gt,
+      false,
     )
   end
 end
 
-function aredpred(
-  tr::TRONTrustRegion,
-  nlp::AbstractNLPModel,
+TRONTrustRegion(::Type{V}, n::Int, Δ₀::T; kwargs...) where {T, V} = TRONTrustRegion(V(undef, n), Δ₀; kwargs...)
+TRONTrustRegion(n::Int, Δ₀::T; kwargs...) where {T} = TRONTrustRegion(Vector{T}, n, Δ₀; kwargs...)
+
+function aredpred!(
+  tr::TRONTrustRegion{T, V},
+  nlp::AbstractNLPModel{T, V},
   f::T,
   f_trial::T,
   Δm::T,
-  x_trial::Vector{T},
-  step::Vector{T},
+  x_trial::V,
+  step::V,
   slope::T,
-) where {T <: AbstractFloat}
-  ared, pred = aredpred(nlp, f, f_trial, Δm, x_trial, step, slope)
+) where {T, V}
+  ared, pred, tr.good_grad = aredpred_common(nlp, f, f_trial, Δm, x_trial, step, tr.gt, slope)
   γ = f_trial - f - slope
-  quad_min = γ <= 0 ? tr.increase_factor : max(tr.large_decrease_factor, -slope / γ / 2)
-  return ared, pred, quad_min
+  tr.quad_min = γ <= 0 ? tr.increase_factor : max(tr.large_decrease_factor, -slope / γ / 2)
+  return ared, pred
 end
 
-function update!(tr::TRONTrustRegion, step_norm::AbstractFloat)
+function update!(tr::TRONTrustRegion{T, V}, step_norm::T) where {T, V}
   α, σ₁, σ₂, σ₃ =
     tr.quad_min, tr.large_decrease_factor, tr.small_decrease_factor, tr.increase_factor
   tr.radius = if tr.ratio < tr.acceptance_threshold
