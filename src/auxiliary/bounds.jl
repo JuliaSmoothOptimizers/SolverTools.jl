f86ba72f4db6665dbf4cf15b8c38030cb6a215c8
tmigot <tangi.migot@gmail.com>
2023-05-12 15:38:06 -0400

    Update src/auxiliary/bounds.jl
    Co-authored-by: Alexis Montoison <35051714+amontoison@users.noreply.github.com>

--- src/auxiliary/bounds.jl
+++ src/auxiliary/bounds.jl
@@ -24,7 +24,7 @@ end
 """
     active!(indices, x, ℓ, u; rtol = 1e-8, atol = 1e-8)
 
-Return a `BitVector` of the active bounds at x, using tolerance `min(rtol * (uᵢ-ℓᵢ), atol)`.
+Update a `BitVector` of the active bounds at x, using tolerance `min(rtol * (uᵢ-ℓᵢ), atol)`.
 If ℓᵢ or uᵢ is not finite, only `atol` is used.
 """
 function active!(
