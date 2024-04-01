0334a3587b8cd8c4e28c2c14d025de3985d6e73b
Jay-sanjay <134289328+Jay-sanjay@users.noreply.github.com>
2024-04-01 17:44:26 +0530

    Added `ARTrustRegion` (#210)
    * added ar-trust-region
    
    ---------
    
    Co-authored-by: tmigot <tangi.migot@gmail.com>

--- test/test_trust_region.jl
+++ test/test_trust_region.jl
@@ -124,4 +124,57 @@
       end
     end
   end
+  
+  struct Workspace
+    ∇fnext::Array{Float64}
+  end
+  @testset "ARTrustRegion" begin
+    Δ₀ = 0.8
+    ar = ARTrustRegion(0.8)
+    @test ar.α₀ == 0.8 
+    @test ar.α == 0.8
+    @test ar.max_α ≈ 1 / sqrt(eps(typeof(0.8)))
+    @test ar.acceptance_threshold == 0.1  
+    @test ar.increase_threshold == 0.75
+    @test ar.reduce_threshold == 0.1
+    @test ar.increase_factor == 5.0
+    @test ar.decrease_factor == 0.1
+    @test ar.large_decrease_factor == 0.01
+    @test ar.max_unsuccinarow == 30
+
+    function NLPModels.grad!(nlp::AbstractNLPModel, x, workspace::Workspace)
+      return grad!(nlp, x, workspace.∇fnext)
+    end
+
+    nlp = BROWNDEN()
+    f = 1.0 
+    Δf = 0.2 
+    Δq = 0.4
+    slope = 0.5 
+    d = [1.0, 2.0, 3.0, 4.0] 
+    xnext = [0.5, 1.5, 2.5, 3.5] 
+    workspace = Workspace([0.8, 1.8, 2.8, 3.8])
+    robust = true
+
+    r, good_grad, gnext = SolverTools.compute_r(nlp, f, Δf, Δq, slope, d, xnext, workspace, robust)
+
+    @test r == 0.5
+    @test good_grad == false
+    @test gnext == [0.8, 1.8, 2.8, 3.8]
+
+    Δq = 1e-13
+    r, good_grad, gnext = SolverTools.compute_r(nlp, f, Δf, Δq, slope, d, xnext, workspace, robust)
+
+    @test r == 3.833532881060353e19
+    @test good_grad == true
+    @test gnext == [-909318.2269441625, -3.4465870681692427e6, 47999.033762642575, -2142.750031496445]
+
+    workspace = Workspace([0.8, 1.8, 2.8, 3.8])
+    robust = false
+    r, good_grad, gnext = SolverTools.compute_r(nlp, f, Δf, Δq, slope, d, xnext, workspace, robust)
+
+    @test r == 2.0e12
+    @test good_grad == false
+    @test gnext == [0.8, 1.8, 2.8, 3.8]
+  end
 end
