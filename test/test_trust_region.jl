46b442b5d24587eb9fe7e9f76cfe20e101408b1a
Tangi Migot <tangi.migot@gmail.com>
2024-04-01 13:43:28 +0200

    Update test_trust_region.jl

--- test/test_trust_region.jl
+++ test/test_trust_region.jl
@@ -142,21 +142,39 @@
     @test ar.large_decrease_factor == 0.01
     @test ar.max_unsuccinarow == 30
 
+    function NLPModels.grad!(nlp::AbstractNLPModel, x, workspace::Workspace)
+      return grad!(nlp, x, workspace.∇fnext)
+    end
 
     nlp = BROWNDEN()
     f = 1.0 
     Δf = 0.2 
-    Δq = 0.3 
+    Δq = 0.4
     slope = 0.5 
-    d = [1.0, 2.0] 
-    xnext = [0.5, 1.5] 
-    workspace = Workspace([0.8, 1.8])
+    d = [1.0, 2.0, 3.0, 4.0] 
+    xnext = [0.5, 1.5, 2.5, 3.5] 
+    workspace = Workspace([0.8, 1.8, 2.8, 3.8])
     robust = true
 
-    r, good_grad, gnext = compute_r(nlp, f, Δf, Δq, slope, d, xnext, workspace, robust)
+    r, good_grad, gnext = SolverTools.compute_r(nlp, f, Δf, Δq, slope, d, xnext, workspace, robust)
 
-    @test r == 0.6666666666666667
+    @test r == 0.5
     @test good_grad == false
-    @test gnext == [0.8, 1.8]
+    @test gnext == [0.8, 1.8, 2.8, 3.8]
+
+    Δq = 1e-13
+    r, good_grad, gnext = SolverTools.compute_r(nlp, f, Δf, Δq, slope, d, xnext, workspace, robust)
+
+    @test r == 3.833532881060353e19
+    @test good_grad == true
+    @test gnext == [-909318.2269441625, -3.4465870681692427e6, 47999.033762642575, -2142.750031496445]
+
+    workspace = Workspace([0.8, 1.8, 2.8, 3.8])
+    robust = false
+    r, good_grad, gnext = SolverTools.compute_r(nlp, f, Δf, Δq, slope, d, xnext, workspace, robust)
+
+    @test r == 2.0e12
+    @test good_grad == false
+    @test gnext == [0.8, 1.8, 2.8, 3.8]
   end
 end
