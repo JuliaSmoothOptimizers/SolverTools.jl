02227abd36ea8ecb1739b7147a8b6f9ae13ed6cb
tmigot <tangi.migot@gmail.com>
2024-05-10 22:16:13 +0200

    fix tests

--- test/test_auxiliary.jl
+++ test/test_auxiliary.jl
@@ -1,15 +1,9 @@
-@testset "Auxiliary tests" begin
+function test_auxiliary()
   @testset "BLAS" begin
     n = 100
-    list_type = [Vector{Float16}, Vector{Float32}, Vector{Float64}, Vector{BigFloat}]
-    if CUDA.functional()
-      list_type = vcat(list_type, CuVector{Float64})
-    end
-
-    for S in list_type
-      T = eltype(S)
-      x = S(rand(100))
-      y = S(rand(100))
+    for T in (Float16, Float32, Float64, BigFloat)
+      x = rand(T, 100)
+      y = rand(T, 100)
       t = one(T) / 2
       s = -one(T) / 4
       @test nrm2(n, x) == norm(x)
@@ -32,60 +26,16 @@
     end
   end
 
-  list_type = [Vector{Float64}]
-  if CUDA.functional()
-    list_type = vcat(list_type, CuVector{Float64})
-  end
-
-  function test_bounds_aux(::Type{S}; skip_allocs = false) where {S}
-    T = eltype(S)
-    x = S([0; 1; 2; 3; 4])
-    ℓ = fill!(S(undef, 5), 0)
-    u = fill!(S(undef, 5), 4)
-    @test active(x, ℓ, u) == [1, 5]
-
-    ind = BitVector(undef, 5)
-    active!(ind, x, ℓ, u)
-    a = @allocated active!(ind, x, ℓ, u)
-    if !skip_allocs
-      @test a == 0
-    end
-
-    d = S([1; 1; 0; -1; -1])
-    @test breakpoints(x, d, ℓ, u) == (4, 3.0, 4.0)
-    a = @allocated breakpoints(x, d, ℓ, u)
-    if !skip_allocs
-      @test a == 0
-    end
-
-    z = S([-rand(2); x[3]; 4 .+ rand(2)])
-    y = S(rand(5))
-    project!(y, z, ℓ, u)
-    @test y == [ℓ[1:2]; x[3]; u[4:5]]
-    a = @allocated project!(y, z, ℓ, u)
-    if !skip_allocs
-      @test a == 0
-    end
-    project_step!(y, x, d, ℓ, u, -one(T))
-    @test y == [0.0; -1.0; 0.0; 1.0; 0.0]
-    a = @allocated project_step!(y, x, d, ℓ, u, -one(T))
-    if !skip_allocs
-      @test a == 0
-    end
-  end
-
-  @testset "Bounds auxiliary" begin
-    test_bounds_aux(Vector{Float64})
-    CUDA.allowscalar() do
-      test_bounds_aux(CuVector{Float64}, skip_allocs = true)
-    end
-  end
-
-  @testset "Compute slope" begin
+  @testset "Bounds" begin
     x = [0.0; 1.0; 2.0; 3.0; 4.0]
     ℓ = zeros(5)
     u = 4 * ones(5)
+    @test active(x, ℓ, u) == [1, 5]
+
     d = [1.0; 1.0; 0.0; -1.0; -1.0]
+    @test breakpoints(x, d, ℓ, u) == (4, 3.0, 4.0)
+    a = @allocated breakpoints(x, d, ℓ, u)
+    @test a == 0
 
     H = diagm(0 => 2 * ones(5), -1 => -ones(4), 1 => -ones(4))
     Hx = zeros(5)
@@ -105,5 +55,47 @@
     @test qx == dot(As, As) / 2 + slope
     b = @allocated compute_As_slope_qs!(As, A, x1, Fx)
     @test b == 0
+
+    z = [ℓ[1:2] - rand(2); x[3]; u[4:5] + rand(2)]
+    y = rand(5)
+    project!(y, z, ℓ, u)
+    @test y == [ℓ[1:2]; x[3]; u[4:5]]
+
+    project_step!(y, x, -d, ℓ, u)
+    @test y == [0.0; -1.0; 0.0; 1.0; 0.0]
+
+    ind = BitVector(undef, 5)
+    active!(ind, x, ℓ, u)
+    a = @allocated active!(ind, x, ℓ, u)
+    @test a == 0
+  end
+
+  if CUDA.functional()
+    CUDA.allowscalar() do
+      @testset "Bounds with CuVector" begin
+        T = Float64
+        S = CuVector{T}
+        x = S([0; 1; 2; 3; 4])
+        ℓ = fill!(S(undef, 5), 0)
+        u = fill!(S(undef, 5), 4)
+        @test active(x, ℓ, u) == [1, 5]
+    
+        d = S([1; 1; 0; -1; -1])
+        @test breakpoints(x, d, ℓ, u) == (4, 3.0, 4.0)
+    
+        z = S([-rand(2); x[3]; 4 .+ rand(2)])
+        y = S(rand(5))
+        project!(y, z, ℓ, u)
+        @test y == [ℓ[1:2]; x[3]; u[4:5]]
+    
+        project_step!(y, x, -d, ℓ, u)
+        @test y == [0.0; -1.0; 0.0; 1.0; 0.0]
+    
+        ind = BitVector(undef, 5)
+        active!(ind, x, ℓ, u)
+      end
+    end
   end
 end
+
+test_auxiliary()
