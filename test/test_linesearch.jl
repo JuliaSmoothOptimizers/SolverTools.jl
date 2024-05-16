db324ed42bc5614326e4d34eab64e54dbf595a6d
tmigot <tangi.migot@gmail.com>
2024-05-16 13:37:33 +0200

    Add CuVector tests for linesearch

--- test/test_linesearch.jl
+++ test/test_linesearch.jl
@@ -1,136 +1,190 @@
+function test_line_model(::Type{S}) where {S}
+  nlp = BROWNDEN(S)
+  n = nlp.meta.nvar
+  x = nlp.meta.x0
+  d = fill!(S(undef, n), -1)
+  lm = LineModel(nlp, x, d)
+  g = fill!(S(undef, n), 0)
+
+  T = eltype(S)
+  @test obj(lm, zero(T)) == obj(nlp, x)
+  @test grad(lm, zero(T)) == dot(grad(nlp, x), d)
+  @test grad!(lm, zero(T), g) == dot(grad(nlp, x), d)
+  @test g == grad(nlp, x)
+  @test derivative(lm, zero(T)) == dot(grad(nlp, x), d)
+  @test derivative!(lm, zero(T), g) == dot(grad(nlp, x), d)
+  @test g == grad(nlp, x)
+  @test objgrad!(lm, one(T), g) == (obj(nlp, x + d), dot(grad(nlp, x + d), d))
+  @test g == grad(nlp, x + d)
+  @test objgrad(lm, zero(T)) == (obj(nlp, x), dot(grad(nlp, x), d))
+  @test hess(lm, zero(T)) ≈ dot(d, hess(nlp, x) * d)
+  @test hess!(lm, zero(T), g) == dot(d, hprod!(nlp, x, d, g))
+
+  @test obj(lm, one(T)) == obj(nlp, x + d)
+  @test grad(lm, one(T)) == dot(grad(nlp, x + d), d)
+  @test hess(lm, one(T)) ≈ dot(d, hess(nlp, x + d) * d)
+
+  @test neval_obj(lm) == 4
+  @test neval_grad(lm) == 7
+  @test neval_hess(lm) == 3
+end
+
+function test_armijo_wolfe(::Type{S}) where {S}
+  T = eltype(S)
+  x0 = fill!(S(undef, 2), 1)
+  nlp = ADNLPModel(x -> x[1]^2 + 4 * x[2]^2, x0, matrix_free = true)
+  d = fill!(S(undef, 2), -1)
+  lm = LineModel(nlp, nlp.meta.x0, d)
+  g = fill!(S(undef, 2), 0)
+
+  t0 = zero(T)
+  t, good_grad, ft, nbk, nbW = armijo_wolfe(lm, obj(lm, t0), grad(lm, t0), g)
+  @test t == 1
+  @test ft == 0
+  @test nbk == 0
+  @test nbW == 0
+
+  redirect!(lm, nlp.meta.x0, fill!(S(undef, 2), -1 // 2))
+  t, good_grad, ft, nbk, nbW = armijo_wolfe(lm, obj(lm, t0), grad(lm, t0), g)
+  @test t == 1
+  @test ft == 1.25
+  @test nbk == 0
+  @test nbW == 0
+
+  redirect!(lm, nlp.meta.x0, fill!(S(undef, 2), -2))
+  t, good_grad, ft, nbk, nbW = armijo_wolfe(lm, obj(lm, t0), grad(lm, t0), g)
+  @test t < 1
+  @test nbk > 0
+  @test nbW == 0
+
+  nlp = ADNLPModel(x -> (x[1] - 1)^2 + 4 * (x[2] - x[1]^2)^2, fill!(S(undef, 2), 0), matrix_free = true)
+  d = S([1.7; 3.2])
+  lm = LineModel(nlp, nlp.meta.x0, d)
+  t, good_grad, ft, nbk, nbW = armijo_wolfe(lm, obj(lm, t0), grad(lm, t0), g)
+  @test t < 1
+  @test nbk > 0
+  @test nbW > 0
+end
+
+function test_armijo_goldstein(::Type{S}) where {S}
+  T = eltype(S)
+  nlp = ADNLPModel(x -> x[1]^2 + 4 * x[2]^2, fill!(S(undef, 2), 1))
+  lm = LineModel(nlp, nlp.meta.x0, fill!(S(undef, 2), -1))
+
+  t0 = zero(T)
+  t, ft, nbk, nbG = armijo_goldstein(lm, obj(lm, t0), grad(lm, t0))
+  @test t == 1
+  @test ft == zero(T)
+  @test nbk == 0
+  @test nbG == 0
+
+  redirect!(lm, nlp.meta.x0, fill!(S(undef, 2), -1 // 2))
+  t, ft, nbk, nbG = armijo_goldstein(lm, obj(lm, t0), grad(lm, t0))
+  @test t == 1
+  @test ft == 1.25
+  @test nbk == 0
+  @test nbG == 0
+
+  redirect!(lm, nlp.meta.x0, fill!(S(undef, 2), -2))
+  t, ft, nbk, nbG = armijo_goldstein(lm, obj(lm, t0), grad(lm, t0))
+  @test t < 1
+  @test nbk > 0
+  @test nbG == 0
+end
+
+function test_armijo_goldstein2(::Type{S}) where {S}
+  T = eltype(S)
+  nlp = ADNLPModel(x -> (x[1] - 1)^2 + 4 * (x[2] - x[1]^2)^2, fill!(S(undef, 2), 0))
+  lm = LineModel(nlp, nlp.meta.x0, S([1.7; 3.2]))
+
+  t0 = zero(T)
+  t, ft, nbk, nbG =
+    armijo_goldstein(lm, obj(lm, t0), grad(lm, t0); t = T(1), τ₀ = T(0.1), τ₁ = T(0.2))
+  @test t < one(T)
+  @test nbk == 4
+  @test nbG == 10
+
+  t, ft, nbk, nbG = armijo_goldstein(
+    lm,
+    obj(lm, t0),
+    grad(lm, t0);
+    t = T(0.001),
+    τ₀ = T(0.1),
+    τ₁ = T(0.2),
+  )
+  @test t < 1.0
+  @test nbk == 2
+  @test nbG == 10
+end
+
 @testset "Linesearch" begin
   @testset "LineModel" begin
-    nlp = BROWNDEN()
-    n = nlp.meta.nvar
-    x = nlp.meta.x0
-    d = -ones(n)
-    lm = LineModel(nlp, x, d)
-    g = zeros(n)
+    test_line_model(Vector{Float64})
+  end
 
-    @test obj(lm, 0.0) == obj(nlp, x)
-    @test grad(lm, 0.0) == dot(grad(nlp, x), d)
-    @test grad!(lm, 0.0, g) == dot(grad(nlp, x), d)
-    @test g == grad(nlp, x)
-    @test derivative(lm, 0.0) == dot(grad(nlp, x), d)
-    @test derivative!(lm, 0.0, g) == dot(grad(nlp, x), d)
-    @test g == grad(nlp, x)
-    @test objgrad!(lm, 1.0, g) == (obj(nlp, x + d), dot(grad(nlp, x + d), d))
-    @test g == grad(nlp, x + d)
-    @test objgrad(lm, 0.0) == (obj(nlp, x), dot(grad(nlp, x), d))
-    @test hess(lm, 0.0) ≈ dot(d, hess(nlp, x) * d)
-    @test hess!(lm, 0.0, g) == dot(d, hprod!(nlp, x, d, g))
-
-    @test obj(lm, 1.0) == obj(nlp, x + d)
-    @test grad(lm, 1.0) == dot(grad(nlp, x + d), d)
-    @test hess(lm, 1.0) ≈ dot(d, hess(nlp, x + d) * d)
-
-    @test neval_obj(lm) == 4
-    @test neval_grad(lm) == 7
-    @test neval_hess(lm) == 3
+  if CUDA.functional()
+    @testset "LineModel with CuArray" begin
+      CUDA.allowscalar() do
+        test_line_model(CuVector{Float64})
+      end
+    end
   end
 
   @testset "Armijo-Wolfe" begin
-    nlp = ADNLPModel(x -> x[1]^2 + 4 * x[2]^2, ones(2))
-    lm = LineModel(nlp, nlp.meta.x0, -ones(2))
-    g = zeros(2)
+    test_armijo_wolfe(Vector{Float64})
+  end
 
-    t, good_grad, ft, nbk, nbW = armijo_wolfe(lm, obj(lm, 0.0), grad(lm, 0.0), g)
-    @test t == 1
-    @test ft == 0.0
-    @test nbk == 0
-    @test nbW == 0
-
-    redirect!(lm, nlp.meta.x0, -ones(2) / 2)
-    t, good_grad, ft, nbk, nbW = armijo_wolfe(lm, obj(lm, 0.0), grad(lm, 0.0), g)
-    @test t == 1
-    @test ft == 1.25
-    @test nbk == 0
-    @test nbW == 0
-
-    redirect!(lm, nlp.meta.x0, -2 * ones(2))
-    t, good_grad, ft, nbk, nbW = armijo_wolfe(lm, obj(lm, 0.0), grad(lm, 0.0), g)
-    @test t < 1
-    @test nbk > 0
-    @test nbW == 0
-
-    nlp = ADNLPModel(x -> (x[1] - 1)^2 + 4 * (x[2] - x[1]^2)^2, zeros(2))
-    lm = LineModel(nlp, nlp.meta.x0, [1.7; 3.2])
-    t, good_grad, ft, nbk, nbW = armijo_wolfe(lm, obj(lm, 0.0), grad(lm, 0.0), g)
-    @test t < 1
-    @test nbk > 0
-    @test nbW > 0
+  if CUDA.functional()
+    @testset "Armijo-Wolfe with CuArray" begin
+      CUDA.allowscalar() do
+        test_armijo_wolfe(CuVector{Float64})
+      end
+    end
   end
 
   @testset "Armijo-Goldstein" begin
-    nlp = ADNLPModel(x -> x[1]^2 + 4 * x[2]^2, ones(2))
-    lm = LineModel(nlp, nlp.meta.x0, -ones(2))
+    @testset "Armijo-Goldstein Float64" begin
+      test_armijo_goldstein(Vector{Float64})
+    end
 
-    T = Float64
-    t, ft, nbk, nbG = armijo_goldstein(lm, obj(lm, 0.0), grad(lm, 0.0))
-    @test t == 1
-    @test ft == zero(T)
-    @test nbk == 0
-    @test nbG == 0
+    @testset "Armijo-Goldstein Float32" begin
+      test_armijo_goldstein2(Vector{Float32})
+    end
 
-    redirect!(lm, nlp.meta.x0, -ones(2) / 2)
-    t, ft, nbk, nbG = armijo_goldstein(lm, obj(lm, 0.0), grad(lm, 0.0))
-    @test t == 1
-    @test ft == 1.25
-    @test nbk == 0
-    @test nbG == 0
-
-    redirect!(lm, nlp.meta.x0, -2 * ones(2))
-    t, ft, nbk, nbG = armijo_goldstein(lm, obj(lm, 0.0), grad(lm, 0.0))
-    @test t < 1
-    @test nbk > 0
-    @test nbG == 0
-
-    T = Float32
-
-    nlp = ADNLPModel(x -> (x[1] - 1)^2 + 4 * (x[2] - x[1]^2)^2, zeros(T, 2))
-    lm = LineModel(nlp, nlp.meta.x0, T.([1.7; 3.2]))
-    t, ft, nbk, nbG =
-      armijo_goldstein(lm, obj(lm, T(0)), grad(lm, T(0)); t = T(1), τ₀ = T(0.1), τ₁ = T(0.2))
-    @test t < one(T)
-    @test nbk == 4
-    @test nbG == 10
-
-    t, ft, nbk, nbG = armijo_goldstein(
-      lm,
-      obj(lm, T(0.0)),
-      grad(lm, T(0.0));
-      t = T(0.001),
-      τ₀ = T(0.1),
-      τ₁ = T(0.2),
-    )
-    @test t < 1.0
-    @test nbk == 2
-    @test nbG == 10
+    if CUDA.functional()
+      @testset "Armijo-Goldstein with CuArray" begin
+        CUDA.allowscalar() do
+          test_armijo_goldstein(CuVector{Float64})
+        end
+      end
+    end
   end
 
   if VERSION ≥ v"1.6"
     @testset "Don't allocate" begin
-      nlp = BROWNDEN()
+      S = Vector{Float64}
+      T = eltype(S)
+      nlp = BROWNDEN(S)
       n = nlp.meta.nvar
       x = nlp.meta.x0
-      g = zeros(n)
-      d = -40 * ones(n)
+      g = fill!(S(undef, n), 0)
+      d = fill!(S(undef, n), -40)
       lm = LineModel(nlp, x, d)
 
-      al = @wrappedallocs obj(lm, 1.0)
+      al = @wrappedallocs obj(lm, one(T))
       @test al == 0
 
-      al = @wrappedallocs grad!(lm, 1.0, g)
+      al = @wrappedallocs grad!(lm, one(T), g)
       @test al == 0
 
-      al = @wrappedallocs objgrad!(lm, 1.0, g)
+      al = @wrappedallocs objgrad!(lm, one(T), g)
       @test al == 0
 
-      al = @wrappedallocs hess!(lm, 1.0, g)
+      al = @wrappedallocs hess!(lm, one(T), g)
       @test al == 0
 
-      h₀ = obj(lm, 0.0)
-      slope = grad(lm, 0.0)
+      h₀ = obj(lm, zero(T))
+      slope = grad(lm, zero(T))
 
       # armijo-wolfe
       (t, gg, ht, nbk, nbW) = armijo_wolfe(lm, h₀, slope, g)
