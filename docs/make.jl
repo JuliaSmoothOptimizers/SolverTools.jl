0015c5297d3d69a936f413564ca9292f548994c6
tmigot <tangi.migot@gmail.com>
2025-08-26 19:55:28 -0400

    Apply BestieTemplate

--- docs/make.jl
+++ docs/make.jl
@@ -1,20 +1,21 @@
-using Documenter, SolverTools
+using SolverTools
+using Documenter
 
-makedocs(
+DocMeta.setdocmeta!(SolverTools, :DocTestSetup, :(using SolverTools); recursive = true)
+
+const page_rename = Dict("developer.md" => "Developer docs") # Without the numbers
+const numbered_pages = [
+  file for
+  file in readdir(joinpath(@__DIR__, "src")) if file != "index.md" && splitext(file)[2] == ".md"
+]
+
+makedocs(;
   modules = [SolverTools],
-  doctest = true,
-  linkcheck = true,
-  strict = true,
-  format = Documenter.HTML(
-    assets = ["assets/style.css"],
-    prettyurls = get(ENV, "CI", nothing) == "true",
-  ),
+  authors = "Dominique Orban <dominique.orban@gerad.ca>, Abel Soares Siqueira <abel.s.siqueira@gmail.com>, Tangi Migot <tangi.migot@gmail.com>",
+  repo = "https://github.com/JuliaSmoothOptimizers/SolverTools.jl/blob/{commit}{path}#{line}",
   sitename = "SolverTools.jl",
-  pages = ["Home" => "index.md", "Reference" => "reference.md"],
+  format = Documenter.HTML(; canonical = "https://JuliaSmoothOptimizers.github.io/SolverTools.jl"),
+  pages = ["index.md"; numbered_pages],
 )
 
-deploydocs(
-  repo = "github.com/JuliaSmoothOptimizers/SolverTools.jl.git",
-  push_preview = true,
-  devbranch = "main",
-)
+deploydocs(; repo = "github.com/JuliaSmoothOptimizers/SolverTools.jl")
